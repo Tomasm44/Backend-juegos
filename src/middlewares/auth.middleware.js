b7581184723b86792c56b14adc8ddcff03703aea
Tomás Briones <tomas.briones6c@gmail.com>
2026-03-29 02:08:23 -0300

    fix

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,14 +1,21 @@
 import jwt from "jsonwebtoken"
 
 export const verificarToken = (req, res, next) => {
-  const token = req.headers.authorization
+  // Usamos 'let' porque vamos a modificar la variable 'token'
+  let token = req.headers.authorization 
 
   if (!token) {
     return res.status(401).json({ message: "No autorizado" })
   }
 
+  // ESTO ES LO QUE FALTABA: Limpiar el prefijo "Bearer "
+  if (token.startsWith("Bearer ")) {
+    token = token.slice(7, token.length);
+  }
+
   try {
-    const data = jwt.verify(token, "secreto")
+    // Aquí "secreto" debe ser igual al que usaste para crear el token
+    const data = jwt.verify(token, "secreto") 
     req.user = data
     next()
   } catch (error) {
